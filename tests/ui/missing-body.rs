e1b04d01bbae93484f2fcd70c3743feee3224cf9
David Tolnay <dtolnay@gmail.com>
2020-03-23 12:29:06 -0700

    Add test showing missing body bug

---
+++ tests/ui/missing-body.rs
@@ -0,0 +1,17 @@
+#![deny(redundant_semicolons)]
+
+use async_trait::async_trait;
+
+#[async_trait]
+trait Trait {
+    async fn f(&self);
+}
+
+struct Thing;
+
+#[async_trait]
+impl Trait for Thing {
+    async fn f(&self);
+}
+
+fn main() {}
