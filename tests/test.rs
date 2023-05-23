486fe7e6de5d767a0ac18cfef394eece6d3bfefe
David Tolnay <dtolnay@gmail.com>
2023-05-23 08:16:48 -0700

    Ignore non_minimal_cfg clippy lint in test
        error: unneeded sub `cfg` when there is no condition
            --> tests/test.rs:1481:66
             |
        1481 |         async fn cfg_param(&self, #[cfg(any())] param: u8, #[cfg(all())] _unused: u8) {}
             |                                                                  ^^^^^
             |
             = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#non_minimal_cfg
             = note: `-D clippy::non-minimal-cfg` implied by `-D clippy::all`
    
        error: unneeded sub `cfg` when there is no condition
            --> tests/test.rs:1483:71
             |
        1483 |         async fn cfg_param_wildcard(&self, #[cfg(any())] _: u8, #[cfg(all())] _: u8) {}
             |                                                                       ^^^^^
             |
             = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#non_minimal_cfg
    
        error: unneeded sub `cfg` when there is no condition
            --> tests/test.rs:1488:19
             |
        1488 |             #[cfg(all())] (_left, _right): (u8, u8),
             |                   ^^^^^
             |
             = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#non_minimal_cfg

--- tests/test.rs
+++ tests/test.rs
@@ -9,6 +9,7 @@
     clippy::missing_panics_doc,
     clippy::missing_safety_doc,
     clippy::needless_return,
+    clippy::non_minimal_cfg,
     clippy::trivially_copy_pass_by_ref,
     clippy::unused_async
 )]
