627124bf67cc173c7564454e71dfccc30f6bc5f5
David Tolnay <dtolnay@gmail.com>
2023-12-30 15:42:46 -0800

    Release 0.1.76

--- src/lib.rs
+++ src/lib.rs
@@ -316,7 +316,7 @@
 //! let object = &value as &dyn ObjectSafe;
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/async-trait/0.1.75")]
+#![doc(html_root_url = "https://docs.rs/async-trait/0.1.76")]
 #![allow(
     clippy::default_trait_access,
     clippy::doc_markdown,
