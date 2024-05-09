74cd34aa767ee46d7ea3dc09bbe092adeaeaeaa0
David Tolnay <dtolnay@gmail.com>
2024-05-08 21:52:06 -0700

    Skip rerunning build script on library code changes

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
     // Warning: build.rs is not published to crates.io.
 
+    println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-check-cfg=cfg(async_trait_nightly_testing)");
 }
