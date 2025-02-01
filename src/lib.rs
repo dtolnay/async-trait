46a70c0a1fa63201ac6429702ba7d82aa97c3f00
David Tolnay <dtolnay@gmail.com>
2025-01-31 18:20:47 -0800

    Release 0.1.86

--- src/lib.rs
+++ src/lib.rs
@@ -214,7 +214,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/async-trait/0.1.85")]
+#![doc(html_root_url = "https://docs.rs/async-trait/0.1.86")]
 #![allow(
     clippy::default_trait_access,
     clippy::doc_markdown,
